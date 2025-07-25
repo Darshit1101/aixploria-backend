646c4e622dc5108f62e46dc40c58634eb0cad8b4
Darshit Kakadiya <darshitkakadiya1@gmail.com>
2025-07-25 10:57:13 +0530

    Update news.route.js

--- routes/news.route.js
+++ routes/news.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const newsController = require('../controller/news.controller');
 
 router.post('/', newsController.createNews);
-router.get('/', newsController.getAllNews);
+router.get('/getAllNews', newsController.getAllNews);
 router.get('/:id', newsController.getNewsById);
 router.delete('/:id', newsController.deleteNews);
 router.put('/:id', newsController.updateNews);
