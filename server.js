313d6b2f38142f1fb762a86974b8d3b39add3b5a
Darshit Kakadiya <darshitkakadiya1@gmail.com>
2025-07-28 11:34:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(express.json());
 
 // Serve uploaded images
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // API routes
 app.use("/api/auth", authRoutes);
